2eace28e2a0dbe5db6d9cbd4257fe9d98a67f724
RapidRoger18 <atharvakashalkar1804@gmail.com>
2025-07-11 10:48:52 +0530

    Adds adaptive RL algorithm for prefetching
    upstream merge

--- prefetcher/adaptive/adaptive.cc
+++ prefetcher/adaptive/adaptive.cc
@@ -1,9 +1,21 @@
 #include <array>
 #include <cmath>
 #include <optional>
+#include <iostream>
 
 #include "adaptive.h"
 #include "cache.h"
+
+adaptive::adaptive(CACHE* cache_ptr)
+: champsim::modules::prefetcher(cache_ptr),
+  cache(cache_ptr),
+  q_table_lru(Q_TABLE_SETS, Q_TABLE_SET_ENTRIES),
+  table(TABLE_SETS, TABLE_SET_ENTRIES)
+{
+    std::cout << "[adaptive] Constructed with:\n";
+    std::cout << "  Q_TABLE_SETS = " << Q_TABLE_SETS << ", Q_TABLE_SET_ENTRIES = " << Q_TABLE_SET_ENTRIES << "\n";
+    std::cout << "  TABLE_SETS   = " << TABLE_SETS   << ", TABLE_SET_ENTRIES   = " << TABLE_SET_ENTRIES << "\n";
+}
 
 void update_confidence(float& confidence, bool detected, bool useful) {
     if (detected && confidence < 0.5f)
@@ -42,7 +54,7 @@ std::vector<int> adaptive::RLState::active_tiles() const {
     return indices;
 }
 
-int select_action(const std::array<float, MAX_ACTIONS>& q_values, float epsilon) {
+int adaptive::select_action(const std::array<float, MAX_ACTIONS>& q_values, float epsilon) {
     float random_val = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);  // Generate a random float value
     if (random_val < epsilon) {
       return rand() % MAX_ACTIONS; // Explore the actions 
@@ -65,16 +77,17 @@ float adaptive::compute_reward(access_type type, uint8_t cache_hit, bool useful_
 }
 
 void adaptive::update_q_value(const QTableEntry& prev_entry, int prev_action, float reward, const QTableEntry& curr_entry,
-                              const std::array<float, MAX_ACTIONS>& curr_q_values, float alpha = 0.1f, float gamma = 0.9f) {
+                              const std::array<float, MAX_ACTIONS>& curr_q_values, float alpha, float gamma) {
     
     auto q_entry = q_table_lru.check_hit(prev_entry);
     std::array<float, MAX_ACTIONS> q_values;
 
     if (q_entry.has_value())
         q_values = q_entry->q_values;
-    else
-        q_values.fill(0.0f);
-
+    else{
+        q_values.fill(0.01f);
+        q_values[no_prefetch] = 0.0f;
+    }
     float max_next_q = *std::max_element(curr_q_values.begin(), curr_q_values.end());
 
     q_values[prev_action] += alpha * (reward + gamma * max_next_q - q_values[prev_action]); //Q(s, a) ← Q(s, a) + α * (reward + γ * max(Q(s’, a’)) - Q(s, a))
@@ -84,18 +97,92 @@ void adaptive::update_q_value(const QTableEntry& prev_entry, int prev_action, fl
     q_table_lru.fill(updated);
 }
 
-void adaptive::issue_stride_prefetch(champsim::address addr, int stride, int degree = 2) { 
-    champsim::address pf_address = addr;
-    bool light_load = intern_->get_mshr_occupancy_ratio() < 0.5; // miss status holding registers (MSHR) 
-    for (int i = 0; i < degree; ++i) {                           //Issue stride prefetch
-        pf_address += stride;
-        if (champsim::page_number{pf_address} != champsim::page_number{addr})
-            break; // avoid crossing page
-        prefetch_line(pf_address, light_load, 0);
+bool adaptive::issue_stride_prefetch(champsim::address addr, int stride, int degree) { 
+    champsim::address pf_addr = addr;
+    if (stride == 0) return true; 
+    bool success = false;
+    bool light_load = intern_->get_mshr_occupancy_ratio() < 0.5;
+    for (int i = 0; i < degree; ++i) {
+        pf_addr += stride;
+        if (champsim::page_number{pf_addr} != champsim::page_number{addr})
+            break;
+        success |= prefetch_line(pf_addr, light_load, 0);
+        if (success) std::cerr << "Issued stride pf at addr=" << pf_addr << " stride=" << stride << "\n";
     }
+    return success;
 }
 
 
+bool adaptive::issue_multi_stride_prefetch(champsim::address ip, champsim::address addr, int degree) {
+    auto it = table.check_hit(RLState{ip});
+    if (!it.has_value()) return false;
+    const RLState& state = it.value();
+    champsim::block_number base_line{addr};
+    bool success = false;
+
+    for (std::size_t i = 0; i + degree < state.stride_history.size(); ++i) {
+        if (state.stride_history[i] == state.last_stride) {
+            champsim::block_number block_addr = base_line;
+
+            for (std::size_t j = 1; j <= degree; ++j) {
+                auto next_stride = state.stride_history[i + j];
+                block_addr += next_stride;
+
+                champsim::address pf_addr{block_addr};
+                if (champsim::page_number{pf_addr} != champsim::page_number{addr})
+                    break;
+
+                bool light_load = intern_->get_mshr_occupancy_ratio() < 0.5;
+                success |= prefetch_line(pf_addr, light_load, 0);
+                if (success) std::cerr << "Issued multi stride pf at addr=" << pf_addr << "\n";
+            }
+            break;
+        }
+    }
+    return success;
+}
+
+bool adaptive::issue_locality_prefetch(champsim::address addr, int degree) {
+    champsim::block_number cl_addr{addr};
+    bool light_load = intern_->get_mshr_occupancy_ratio() < 0.5;
+    bool success = false;
+    for (int i = 1; i <= degree; ++i) {
+        champsim::address pf_addr{cl_addr + i};
+        if (champsim::page_number{pf_addr} != champsim::page_number{addr})
+            break;
+        success |= prefetch_line(pf_addr, light_load, 0);
+        if (success) std::cerr << "Issued locality pf at addr=" << pf_addr << "\n";
+    }
+    for (int i = 1; i <= degree; ++i) {
+        champsim::address pf_addr{cl_addr - i};
+        if (champsim::page_number{pf_addr} != champsim::page_number{addr})
+            break;
+        success |= prefetch_line(pf_addr, light_load, 0);
+        if (success) std::cerr << "Issued locality pf at addr=" << pf_addr << "\n";
+    }
+    return success;
+}
+
+bool adaptive::issue_correlation_prefetch(champsim::address ip, champsim::address addr) {
+    auto it = table.check_hit(RLState{ip});
+    bool light_load = intern_->get_mshr_occupancy_ratio() < 0.5;
+    bool success = false;
+    if (!it.has_value()) return false;
+    const RLState& state = it.value();
+    if (state.recent_addr.size() < 2)
+        return true;
+    for (size_t i = 0; i + 1 < state.recent_addr.size(); ++i) {
+        if (state.recent_addr[i] == champsim::block_number{addr}) {
+            champsim::block_number next = state.recent_addr[i + 1];
+            if (champsim::page_number{next} == champsim::page_number{addr}) {
+                success |= prefetch_line(champsim::address{next}, light_load, 0);
+                if (success) std::cerr << "Issued correlation pf at addr=" << next << "\n";
+            }
+            break;
+        }
+    }
+    return success;
+}
 uint32_t adaptive::prefetcher_cache_operate(champsim::address addr, champsim::address ip, uint8_t cache_hit, bool useful_prefetch, access_type type, uint32_t metadata_in){
   // SO we need to observe the pattern of access through this function
 /*
@@ -108,6 +195,9 @@ uint32_t adaptive::prefetcher_cache_operate(champsim::address addr, champsim::ad
 | `type`            | Type of access: **demand**, **prefetch**, **writeback**, etc.        |
 | `metadata_in`     | Info passed along from previous events (usually just passed through) |
 */ 
+
+  //std::cerr << "Entered prefetcher_cache_operate" << std::endl;
+
   champsim::block_number cl_addr{addr};
   champsim::block_number::difference_type stride = 0;
   RLState state;
@@ -123,12 +213,15 @@ uint32_t adaptive::prefetcher_cache_operate(champsim::address addr, champsim::ad
     state.last_cl_addr = cl_addr;
     std::rotate(state.stride_history.begin(), state.stride_history.begin() + 1, state.stride_history.end());
     state.stride_history.back() = stride;
+    std::rotate(state.recent_addr.begin(), state.recent_addr.begin() + 1, state.recent_addr.end());
+    state.recent_addr.back() = cl_addr;
   }
   else{
     state.ip = ip;
     state.last_cl_addr = cl_addr;
     state.last_stride = stride;
     state.stride_history.back() = stride;
+    state.recent_addr.back() = cl_addr;
   }
   table.fill(state);
 
@@ -143,21 +236,8 @@ uint32_t adaptive::prefetcher_cache_operate(champsim::address addr, champsim::ad
   }
 
   int action = select_action(q_values, 0.2f);  // Take a action 20% explore
-
-  switch (static_cast<PrefetchActions>(action)) {
-    case no_prefetch:// Do nothing
-      break;
-    case simple_stride: issue_stride_prefetch(addr, stride); 
-      break;
-    case multi_stride: issue_multi_stride_prefetch(addr);
-      break;
-    case locality: issue_locality_prefetch(addr);
-      break;
-    case correlation: issue_correlation_prefetch(addr);
-      break;
-    default:// fallback
-      break;
-  }
+  PrefetchTask task{ip, addr, action, stride};
+  prefetch_queue.push_back(task);
   // assign reward and update the q table 
   if (prev_action != -1) {
       float reward = compute_reward(type, cache_hit, useful_prefetch);
@@ -171,7 +251,7 @@ uint32_t adaptive::prefetcher_cache_operate(champsim::address addr, champsim::ad
   return metadata_in;
 }
 
-uint32_t prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in){
+uint32_t adaptive::prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in){
 /*
 | Parameter      | Meaning                                                               |
 | -------------- | --------------------------------------------------------------------- |
@@ -181,8 +261,42 @@ uint32_t prefetcher_cache_fill(champsim::address addr, long set, long way, uint8
 | `evicted_addr` | If something was **evicted** to make space, this is its address       |
 | `metadata_in`  | Metadata returned from `cache_operate` — can be used for tracking     |
 */
+  return metadata_in;
 }
 
-void prefetcher_cycle_operate(){
+void adaptive::prefetcher_cycle_operate(){
+
+    //std::cerr << "Entered prefetcher_cycle_operate" << std::endl;
+    
+    int issued = 0;
+    while (!prefetch_queue.empty() && issued < 1) {
+        PrefetchTask& task = prefetch_queue.front(); 
+        bool success = false;
+        switch (static_cast<PrefetchActions>(task.action)) {
+            case no_prefetch:
+                success = true; // no work to do
+                break;
+            case simple_stride:
+                success = issue_stride_prefetch(task.addr, task.stride);
+                break;
+            case multi_stride:
+                success = issue_multi_stride_prefetch(task.ip, task.addr);
+                break;
+            case locality:
+                success = issue_locality_prefetch(task.addr);
+                break;
+            case correlation:
+                success = issue_correlation_prefetch(task.ip,task.addr);
+                break;
+            default:
+                break;
+        }
+        if (success) {
+            prefetch_queue.pop_front();  // remove once prefetch was handled
+            ++issued;
+        } else {
+            break; // MSHR is full or something failed, try again next cycle
+        }
+    }
   //Only useful if you want to decay confidence, update timers, or do background scanning every cycle. 
 }
--- prefetcher/adaptive/adaptive.h
+++ prefetcher/adaptive/adaptive.h
@@ -21,37 +21,73 @@ constexpr int MAX_ACTIONS = 5;
 
 struct adaptive : public champsim::modules::prefetcher {
 
-    adaptive(CACHE* cache);
-
+    explicit adaptive(CACHE* cache);
     // Prefetcher API Hooks
     uint32_t prefetcher_cache_operate(champsim::address addr, champsim::address ip, uint8_t cache_hit, bool useful_prefetch, access_type type, uint32_t metadata_in);
-    // Called every time the CPU accesses the cache. Can be used to record data
     uint32_t prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in);
-    // Called when a block (line) is inserted into the cache. Can be demand miss or a prefetch was successful
     void prefetcher_cycle_operate();
-    // Called every cycle, regardless of accesses.
 
-
+    constexpr static std::size_t Q_TABLE_SETS = 512;
+    constexpr static std::size_t Q_TABLE_SET_ENTRIES = 8;
+    constexpr static std::size_t TABLE_SETS = 256;   // There are 256 sets (buckets).
+    constexpr static std::size_t TABLE_SET_ENTRIES = 4; // Each set can store up to 4 entries.
+    // When a 5th IP maps to the same set, one of the 4 entries is evicted (typically Least Recently Used — LRU).
 
 struct RLState {
     static constexpr size_t max_history = 4;
-    static constexpr size_t max_recent_blocks = 8;
+    static constexpr size_t max_recent_addr = 8;
 
     champsim::address ip{};
     champsim::block_number last_cl_addr{};
     champsim::block_number::difference_type last_stride{};
     std::array<champsim::block_number::difference_type, max_history> stride_history{};
-    std::array<uint64_t, max_recent_blocks> access_timestamps{};
+    std::array<champsim::block_number, max_recent_addr> recent_addr{};
 
     std::vector<int> active_tiles() const;
-    int last_action = 0;
-    access_type last_access_type{};
-    bool was_cache_hit = false;
-    uint8_t prefetch_count = 0;
-    float last_prefetch_useful = 0.0;
-    size_t total_prefetches = 0;
-    size_t useful_prefetches = 0;
+    int last_action;
+
+    auto index() const
+    {
+      using namespace champsim::data::data_literals;
+      return ip.slice_upper<2_b>();
+    }
+    auto tag() const
+    {
+      using namespace champsim::data::data_literals;
+      return ip.slice_upper<2_b>();
+    }
+
 };
+
+struct QTableEntry {
+    std::vector<int> key;                            // Active tile indices (state encoding)
+    std::array<float, MAX_ACTIONS> q_values{};       // One Q-value per action
+
+    // Determines which set (bucket) this entry maps to.
+    std::size_t index() const {
+        std::size_t idx = 0;
+        for (size_t i = 0; i < key.size(); ++i)
+            idx += static_cast<std::size_t>(key[i]) * (i + 1);
+        return idx % Q_TABLE_SETS;
+    }
+    std::vector<int> tag() const {
+        return key;
+    }
+    bool operator==(const QTableEntry& other) const {
+        return key == other.key;
+    }
+};
+    champsim::msl::lru_table<QTableEntry> q_table_lru;
+    champsim::msl::lru_table<RLState> table;
+
+     
+struct PrefetchTask {
+    champsim::address ip;
+    champsim::address addr;
+    int action;
+    int stride; // for stride-based prefetching
+};
+std::deque<PrefetchTask> prefetch_queue;
 
 enum PrefetchActions{
     no_prefetch = 0,
@@ -61,58 +97,17 @@ enum PrefetchActions{
     correlation = 4
 };
 
-struct QTableEntry {
-    std::vector<int> key;                            // Active tile indices
-    std::array<float, MAX_ACTIONS> q_values{};       // One Q-value per action
+    bool issue_stride_prefetch(champsim::address addr, int stride, int degree = 2); // further implementation after the action is decided 
+    bool issue_multi_stride_prefetch(champsim::address ip, champsim::address addr, int degree = 3);
+    bool issue_locality_prefetch(champsim::address addr, int degree = 2);
+    bool issue_correlation_prefetch(champsim::address ip, champsim::address addr);
 
-    size_t index() const {
-        // Hash to determine which set this goes into 
-        size_t hash = 0;
-        for (int val : key)
-            hash ^= std::hash<int>{}(val) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-        return hash;
-    }
-    /* Similar to boosts hash combining
-    | Part                    | Meaning                                                     |
-    | ----------------------- | ----------------------------------------------------------- |
-    | `std::hash<int>{}(val)` | Hashes individual tile index                                |
-    | `0x9e3779b9`            | Golden ratio constant in hex (≈ 2^32 / φ) to spread entropy | magic number from Fibonacci hashing(inverse of golden ratio)
-    | `hash << 6` and `>> 2`  | Bit-shifts to mix up bits                                   |
-    | `^=` (XOR assignment)   | Combines the new hashed tile with the cumulative hash       |
-    */
-
-    std::vector<int> tag() const {
-        return key; // Match full key for exact hit
-    }
-
-    // Required for comparison in tag() logic
-    bool operator==(const QTableEntry& other) const {
-        return key == other.key;
-    }
-};
-    constexpr static std::size_t Q_TABLE_SETS = 512;
-    constexpr static std::size_t Q_TABLE_SET_ENTRIES = 8;
-    constexpr static std::size_t TABLE_SETS = 256;   // There are 256 sets (buckets).
-    constexpr static std::size_t TABLE_SET_ENTRIES = 4; // Each set can store up to 4 entries.
-    // When a 5th IP maps to the same set, one of the 4 entries is evicted (typically Least Recently Used — LRU).
-
-    champsim::msl::lru_table<QTableEntry> q_table_lru{Q_TABLE_SETS, Q_TABLE_SET_ENTRIES};
-    champsim::msl::lru_table<RLState> table{TABLE_SETS, TABLE_SET_ENTRIES};
-    
-    std::unordered_map<champsim::block_number, champsim::block_number> correlation_table;
-
+  private:
     // Helper Functions
+    CACHE* cache;
     int select_action(const std::array<float, MAX_ACTIONS>& q_values, float epsilon);  // select actions 
     float compute_reward(access_type type, uint8_t cache_hit, bool useful_prefetch);   // compute reward based on useful prefetch
     void update_q_value(const QTableEntry& prev_entry, int prev_action, float reward, const QTableEntry& curr_entry, 
                         const std::array<float, MAX_ACTIONS>& curr_q_values, float alpha = 0.1f, float gamma = 0.9f); // update the q-value with reward
-    void adaptive::issue_stride_prefetch(champsim::address addr, int stride, int degree = 2); // further implementation after the action is decided 
-    void issue_multi_stride_prefetch(champsim::address addr);
-    void issue_locality_prefetch(champsim::address addr);
-    void issue_correlation_prefetch(champsim::address addr);
-
-  private:
-    void update_reward(const RLState& state, bool useful);
-    void train_q_table(const RLState& state, int8_t reward);
 };
 #endif
